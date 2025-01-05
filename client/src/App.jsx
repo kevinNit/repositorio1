0db5373630cde09c8024e15aedf5674b56aa9d20
bryan <programador0000@gmail.com>
2025-01-05 03:06:16 -0500

    actualizar4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
 function App() {
+  console.log("API URL:", process.env.REACT_APP_API_URL); // Agregar esta línea
   return (
     <div className="container my-5">
       <h1 className="text-center mb-4">Lista de Tareas (MERN)</h1>
